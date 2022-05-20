8849f0cedde940494edbc8e5e17cfa5fba9cc15b
mieenevoldsen <91202375+mieenevoldsen@users.noreply.github.com>
2022-05-20 09:44:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     setTimeout(function () {
       // .speech-bubble
      document.querySelector(".speech-bubble").style.display = "block";  
-    }, 1300);
+    }, 400);
   })();
 //Sound
 function play(){
@@ -13,9 +13,9 @@ function play(){
 }
 
 
-//Fade in
+//Fade in (chokoladekage med kokos)
 const text = document.querySelector(".intro");
-const strText = "Hej med dig, så du vil gerne lave regnbue muffins? Mit navn er Berta og det kan jeg hjælpe dig med. Sørg for at du har alle ingredienserne. Du kan tjekke firkanten af når du har fundet det du skal bruge. Du kan også spørge om en voksen vil printe opskriften ud for dig. Når du har fundet det hele frem så kan vi gå i gang! Klik på videoen nedenunder";
+const strText = "Hej med dig, så du vil gerne lave regnbue muffins? Mit navn er Berta og det kan jeg hjælpe dig med. Sørg for at du har alle ingredienserne. Du kan tjekke firkanten af når du har fundet det du skal bruge. Du kan også spørge om en voksen vil printe opskriften ud for dig. Når du har fundet det hele frem så kan vi gå i gang! Klik på videoen nedenunder.";
 const splitText = strText.split("");
 for (let i = 0; i < splitText.length; i++) {
     text.innerHTML += "<span>" + splitText[i] + "</span>";
@@ -23,6 +23,7 @@ for (let i = 0; i < splitText.length; i++) {
 
 function show(){ 
     document.querySelector(".klik").style.display = "none";
+    document.querySelector(".intro").style.display = "block";
     
     let char = 0;
     let timer = setInterval(onTick, 50);
@@ -39,4 +40,9 @@ function show(){
       clearInterval(timer);
       timer = null;
     }
+}
+
+function start(){
+  show()
+  play();
 }
